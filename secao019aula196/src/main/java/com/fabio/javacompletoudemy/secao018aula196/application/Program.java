81374bd3ed314c7453e37b4ffb10433d4227b082
Fabio Massaretto <>
2020-01-13 13:25:53 -0300

    Wrote classes section 18 e 19

---
+++ secao019aula196/src/main/java/com/fabio/javacompletoudemy/secao018aula196/application/Program.java
@@ -0,0 +1,24 @@
+package com.fabio.javacompletoudemy.secao018aula196.application;
+
+import com.fabio.javacompletoudemy.secao018aula196.service.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many value to add?");
+        int n = sc.nextInt();
+
+        PrintService<String> ps = new PrintService<>();
+
+        for (int i = 0; i < n; i++){
+            String value = sc.next();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+    }
+}
---
+++ secao019aula197/src/main/java/com/fabio/javacompletoudemy/secao018aula197/service/CalculationService.java
@@ -0,0 +1,20 @@
+package com.fabio.javacompletoudemy.secao018aula197.service;
+
+import java.util.List;
+
+public class CalculationService {
+
+    public static <T extends Comparable<T>> T max(List<T> list){
+        if(list.isEmpty()){
+            throw new IllegalStateException("List can't be empty.");
+        }
+        T max = list.get(0);
+
+        for (T item : list){
+            if (item.compareTo(max) > 0){
+                max = item;
+            }
+        }
+        return max;
+    }
+}
